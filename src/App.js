3a41d60f65dcd1816bc043369648603eaf759dd6
AustynYou <austyn.you@gmail.com>
2023-02-14 05:35:34 -0800

    🚧 Custom Action Creation: Reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./styles.css";
+import { useDispatch } from 'react-redux';
+import { reset } from './store/actions';
 import MoviePlaylist from "./components/MoviePlaylist";
 import SongPlaylist from "./components/SongPlaylist";
 
+import "./styles.css";
+
 export default function App() {
+  const dispatch = useDispatch();
   const handleResetClick = () => {
-    //
+    dispatch(reset());
   };
 
   return (
